07f541ebe4822c2b9543c086d4a10d87108c35ed
iamsonghee <65877281+iamsonghee@users.noreply.github.com>
2021-03-16 21:48:18 +0900

    ADD : Product 컴포넌트 레이아웃

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import './Product.scss';
 
 class Product extends Component {
   render() {
-    return <div></div>;
+    return (
+      <product>
+        <div className="item">
+          <div className="itemImgBox">
+            <img
+              src="https://store.baemin.com/data/goods/20/10/44/311/311_detail_02.jpg"
+              alt="camera"
+            />
+            <div className="itemTags">
+              <span className="sale">SALE</span>
+              <span className="best">BEST</span>
+              <span className="new">NEW</span>
+            </div>
+          </div>
+          <div className="itemInfo">
+            <div className="discountRate">20%</div>
+            <div className="itemName">을지로 일회용캬메라. 잘 나왔다</div>
+            <div className="itemBeforePrice">
+              19,000<span>원</span>
+            </div>
+            <div className="itemFinalPrice">
+              15,200<span>원</span>
+            </div>
+          </div>
+        </div>
+      </product>
+    );
   }
 }
 
